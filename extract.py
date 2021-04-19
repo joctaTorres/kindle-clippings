a40deeb0d303f7abadb7bb8c74a615a8eec5513e
Joctã Torres <jocta.torres@gmail.com>
2021-04-18 21:30:04 -0300

    parse file line by line

---
+++ extract.py
@@ -0,0 +1,33 @@
+from io import BufferedIOBase
+import re
+from itertools import zip_longest
+
+
+CLIPPING_SEPARATOR = "=========="
+BOOK_AUTHOR_REGEX = re.compile(r"^(?P<book>.+)\s\((?P<author>.+)\)$")
+HIGHLIGHT_DESCRIPTION = re.compile(r"^\-\s(?P<description>.+?)\s\|\s((?P<location>.+)\s\|\s)?(?P<date>.+)$")
+CLIPPING_SIZE_LINES = 5
+
+
+
+def grouper(n, iterable, fillvalue=None):
+    args = [iter(iterable)] * n
+    return zip_longest(fillvalue=fillvalue, *args)
+
+
+def parse(file):
+    with open(file) as clippings:
+        count = 0
+        for title, description, _, clip, _ in grouper(CLIPPING_SIZE_LINES, clippings):
+            print(title)
+            print(description)
+            print(clip)
+            count +=1
+
+            if count >= 5:
+                break
+
+
+
+if __name__ == "__main__":
+    parse("highlights")
